5f4e2c3a17453622eee14b8e7ddb37bd512620d1
John Shahid <jshahid@pivotal.io>
2016-01-19 14:22:09 -0500

    adds support for gradle based projects, such as UAA
    [#111612642]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- cf-release-submodules.go
+++ cf-release-submodules.go
@@ -1,8 +1,9 @@
 package main
 
 type CfReleaseSubmodules[]struct {
-	Name   string
-	Path string
+	Name		string
+	Path		string
+	LtsPath string
 }
 
 func (c CfReleaseSubmodules) Paths() []string {
@@ -13,15 +14,30 @@ func (c CfReleaseSubmodules) Paths() []string {
 	return p
 }
 
+func (c CfReleaseSubmodules) LtsPaths() []string {
+	p := make([]string, len(c))
+	for k, v := range c {
+		p[k] = v.LtsPath
+	}
+	return p
+}
+
 func NewCfReleaseSubmodules() CfReleaseSubmodules{
 	return CfReleaseSubmodules{
 		{
 			Name:   "cloud_controller_ng",
 			Path:   "src/cloud_controller_ng",
+			LtsPath:   "src/cloud_controller_ng",
 		},
 		{
 			Name:   "loggregator",
 			Path:   "src/loggregator",
+			LtsPath:   "src/loggregator",
+		},
+		{
+			Name:   "uaa",
+			Path:   "src/uaa-release/src/uaa",
+			LtsPath:   "src/uaa",
 		},
 	}
 }
