a517961c1ae1ade5c368b4ee8eb9b342d9cc4bb6
John Shahid <jshahid@pivotal.io>
2016-02-08 13:32:52 -0500

    Scans diego-release projects
    [#112989031]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

---
+++ diego-release-submodules.go
@@ -0,0 +1,131 @@
+package main
+
+type DiegoReleaseSubmodules []struct {
+	Name    string // the name of the project
+	Path    string // the path of the project in the latest release
+}
+
+func (c DiegoReleaseSubmodules) Paths() []string {
+	p := make([]string, len(c))
+	for k, v := range c {
+		p[k] = v.Path
+	}
+	return p
+}
+
+func NewDiegoReleaseSubmodules() DiegoReleaseSubmodules {
+	return DiegoReleaseSubmodules{
+		{
+			Name:    "diego-smoke-tests",
+			Path:    "src/github.com/cloudfoundry-incubator/diego-smoke-tests",
+		},
+		{
+			Name:    "diego-acceptance-tests",
+			Path:    "src/github.com/cloudfoundry-incubator/diego-acceptance-tests",
+		},
+		{
+			Name:    "auction",
+			Path:    "src/github.com/cloudfoundry-incubator/auction",
+		},
+		{
+			Name:    "auctioneer",
+			Path:    "src/github.com/cloudfoundry-incubator/auctioneer",
+		},
+		{
+			Name:    "bbs",
+			Path:    "src/github.com/cloudfoundry-incubator/bbs",
+		},
+		{
+			Name:    "benchmark-bbs",
+			Path:    "src/github.com/cloudfoundry-incubator/benchmark-bbs",
+		},
+		{
+			Name:    "file-server",
+			Path:    "src/github.com/cloudfoundry-incubator/file-server",
+		},
+		{
+			Name:    "executor",
+			Path:    "src/github.com/cloudfoundry-incubator/executor",
+		},
+		{
+			Name:    "stager",
+			Path:    "src/github.com/cloudfoundry-incubator/stager",
+		},
+		{
+			Name:    "rep",
+			Path:    "src/github.com/cloudfoundry-incubator/rep",
+		},
+		{
+			Name:    "nsync",
+			Path:    "src/github.com/cloudfoundry-incubator/nsync",
+		},
+		{
+			Name:    "cf-debug-server",
+			Path:    "src/github.com/cloudfoundry-incubator/cf-debug-server",
+		},
+		{
+			Name:    "converger",
+			Path:    "src/github.com/cloudfoundry-incubator/converger",
+		},
+		{
+			Name:    "route-emitter",
+			Path:    "src/github.com/cloudfoundry-incubator/route-emitter",
+		},
+		{
+			Name:    "routing-info",
+			Path:    "src/github.com/cloudfoundry-incubator/routing-info",
+		},
+		{
+			Name:    "runtime-schema",
+			Path:    "src/github.com/cloudfoundry-incubator/runtime-schema",
+		},
+		{
+			Name:    "locket",
+			Path:    "src/github.com/cloudfoundry-incubator/locket",
+		},
+		{
+			Name:    "tps",
+			Path:    "src/github.com/cloudfoundry-incubator/tps",
+		},
+		{
+			Name:    "cf_http",
+			Path:    "src/github.com/cloudfoundry-incubator/cf_http",
+		},
+		{
+			Name:    "buildpack_app_lifecycle",
+			Path:    "src/github.com/cloudfoundry-incubator/buildpack_app_lifecycle",
+		},
+		{
+			Name:    "docker_app_lifecycle",
+			Path:    "src/github.com/cloudfoundry-incubator/docker_app_lifecycle",
+		},
+		{
+			Name:    "diegocanaryapp",
+			Path:    "src/github.com/cloudfoundry-incubator/diegocanaryapp",
+		},
+		{
+			Name:    "consuladapter",
+			Path:    "src/github.com/cloudfoundry-incubator/consuladapter",
+		},
+		{
+			Name:    "diego-ssh",
+			Path:    "src/github.com/cloudfoundry-incubator/diego-ssh",
+		},
+		{
+			Name:    "cacheddownloader",
+			Path:    "src/github.com/cloudfoundry-incubator/cacheddownloader",
+		},
+		{
+			Name:    "healthcheck",
+			Path:    "src/github.com/cloudfoundry-incubator/healthcheck",
+		},
+		{
+			Name:    "cc-uploader",
+			Path:    "src/github.com/cloudfoundry-incubator/cc-uploader",
+		},
+		{
+			Name:    "vizzini",
+			Path:    "src/github.com/cloudfoundry-incubator/vizzini",
+		},
+	}
+}
