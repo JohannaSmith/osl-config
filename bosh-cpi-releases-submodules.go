2c56cd7f94f434168e160cc5764d0dea855ff0ad
Amin Jamali <ajamali@pivotal.io>
2016-02-16 15:19:10 -0500

    rename bosh-release to bosh-cpi-releases
    Signed-off-by: John Shahid <jshahid@pivotal.io>

--- bosh-cpi-releases-submodules.go
+++ bosh-cpi-releases-submodules.go
@@ -1,30 +1,30 @@
 package main
 
-type BoshReleaseSubmodules []struct {
+type BoshCpiReleasesSubmodules []struct {
 	Name     string // the name of the project
 	Path     string
 	RepoName string
 	RepoURL  string
 }
 
-func NewBoshReleaseSubmodules() BoshReleaseSubmodules {
-	return BoshReleaseSubmodules{
+func NewBoshCpiReleasesSubmodules() BoshCpiReleasesSubmodules {
+	return BoshCpiReleasesSubmodules{
 		{
 			Name:     "bosh-aws-cpi",
-			RepoName: "bosh-aws-cpi",
 			Path:     "src/bosh_aws_cpi",
+			RepoName: "bosh-aws-cpi",
 			RepoURL:  "https://github.com/cloudfoundry-incubator/bosh-aws-cpi-release",
 		},
 		{
 			Name:     "bosh-vcloud-cpi",
-			RepoName: "bosh-vcloud-cpi",
 			Path:     "src/bosh_vcloud_cpi",
+			RepoName: "bosh-vcloud-cpi",
 			RepoURL:  "https://github.com/cloudfoundry-incubator/bosh-vcloud-cpi-release",
 		},
 		{
 			Name:     "bosh-vsphere-cpi",
-			RepoName: "bosh-vsphere-cpi",
 			Path:     "src/vsphere_cpi",
+			RepoName: "bosh-vsphere-cpi",
 			RepoURL:  "https://github.com/cloudfoundry-incubator/bosh-vsphere-cpi-release",
 		},
 	}
