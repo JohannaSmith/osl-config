d861987e36a208c1b0ec35bcdb4a3806a6a47abe
John Shahid <jshahid@pivotal.io>
2016-02-17 13:46:52 -0500

    Adds apps manager release
    [#113468173]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

---
+++ apps-manager-release-submodules.go
@@ -0,0 +1,27 @@
+package main
+
+type AppsManagerReleaseSubmodules []struct {
+	Name string // the name of the project
+	Path string // the path of the project in the latest release
+}
+
+func (c AppsManagerReleaseSubmodules) Paths() []string {
+	p := make([]string, len(c))
+	for k, v := range c {
+		p[k] = v.Name + ":" + v.Path
+	}
+	return p
+}
+
+func NewAppsManagerReleaseSubmodules() AppsManagerReleaseSubmodules {
+	return AppsManagerReleaseSubmodules{
+		{
+			Name: "apps-manager-console",
+			Path: "src/console",
+		},
+		{
+			Name: "app-usage-service",
+			Path: "src/app-usage-service",
+		},
+	}
+}
