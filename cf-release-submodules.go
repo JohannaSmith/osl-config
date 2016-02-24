3bb07badcb9178f75bc4c487fb71f39c765bdad8
Micah Young <myoung@pivotal.io>
2016-02-24 11:38:15 -0500

    Remove the cf-routing-release from cf-release
    cf-release has removed cf-routing-release from their submodules. We
    refactored the configuration for easier abstraction of the LTS releases.
    LTS pipeline can now be easily deleted by the deleting the two LTS
    functions.
    
    [#114400489]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- cf-release-submodules.go
+++ cf-release-submodules.go
@@ -1,9 +1,8 @@
 package main
 
 type CfReleaseSubmodules []struct {
-	Name    string // the name of the project
-	Path    string // the path of the project in the latest release
-	LtsPath string // the path in the lts release
+	Name string // the name of the project
+	Path string // the path of the project in the latest release
 }
 
 func (c CfReleaseSubmodules) Paths() []string {
@@ -18,17 +17,85 @@ func (c CfReleaseSubmodules) LtsPaths() []string {
 	lts := c.Lts()
 	p := make([]string, len(lts))
 	for k, v := range lts {
-		p[k] = v.Name + ":" + v.LtsPath
+		p[k] = v.Name + ":" + v.Path
 	}
 	return p
 }
 
 func (c CfReleaseSubmodules) Lts() CfReleaseSubmodules {
-	var out CfReleaseSubmodules
-	for _, v := range c {
-		if v.LtsPath != "" {
-			out = append(out, v)
-		}
+	out := CfReleaseSubmodules{
+		{
+			Name: "cloud_controller_ng",
+			Path: "src/cloud_controller_ng",
+		},
+		{
+			Name: "loggregator",
+			Path: "src/loggregator",
+		},
+		{
+			Name: "collector",
+			Path: "src/collector",
+		},
+		{
+			Name: "uaa",
+			Path: "src/uaa",
+		},
+		{
+			Name: "gorouter",
+			Path: "src/github.com/cloudfoundry/gorouter",
+		},
+		{
+			Name: "dea_next",
+			Path: "src/dea_next",
+		},
+		{
+			Name: "smoke-tests",
+			Path: "src/smoke-tests",
+		},
+		{
+			Name: "cf-acceptance-tests",
+			Path: "src/github.com/cloudfoundry/cf-acceptance-tests/",
+		},
+		{
+			Name: "statsd-injector",
+			Path: "src/statsd-injector",
+		},
+		{
+			Name: "hm9000",
+			Path: "src/hm9000",
+		},
+		{
+			Name: "cf-routing-release",
+			Path: "src/github.com/cloudfoundry-incubator/routing-api",
+		},
+		{
+			Name: "nats",
+			Path: "src/nats",
+		},
+		{
+			Name: "route-registrar",
+			Path: "src/route_registrar",
+		},
+		{
+			Name: "etcd",
+			Path: "src/github.com/coreos/etcd",
+		},
+		{
+			Name: "etcd-metrics-server",
+			Path: "src/etcd-metrics-server",
+		},
+		{
+			Name: "warden",
+			Path: "src/warden/warden",
+		},
+		{
+			Name: "warden-client",
+			Path: "src/warden/warden-client",
+		},
+		{
+			Name: "warden-protocol",
+			Path: "src/warden/warden-protocol",
+		},
 	}
 	return out
 }
@@ -36,98 +103,76 @@ func (c CfReleaseSubmodules) Lts() CfReleaseSubmodules {
 func NewCfReleaseSubmodules() CfReleaseSubmodules {
 	return CfReleaseSubmodules{
 		{
-			Name:    "cloud_controller_ng",
-			Path:    "src/cloud_controller_ng",
-			LtsPath: "src/cloud_controller_ng",
+			Name: "cloud_controller_ng",
+			Path: "src/cloud_controller_ng",
 		},
 		{
-			Name:    "loggregator",
-			Path:    "src/loggregator",
-			LtsPath: "src/loggregator",
+			Name: "loggregator",
+			Path: "src/loggregator",
 		},
 		{
-			Name:    "collector",
-			Path:    "src/collector",
-			LtsPath: "src/collector",
+			Name: "collector",
+			Path: "src/collector",
 		},
 		{
-			Name:    "uaa",
-			Path:    "src/uaa-release/src/uaa",
-			LtsPath: "src/uaa",
+			Name: "uaa",
+			Path: "src/uaa-release/src/uaa",
 		},
 		{
-			Name:    "gorouter",
-			Path:    "src/github.com/cloudfoundry/gorouter",
-			LtsPath: "src/github.com/cloudfoundry/gorouter",
+			Name: "gorouter",
+			Path: "src/github.com/cloudfoundry/gorouter",
 		},
 		{
-			Name:    "dea_next",
-			Path:    "src/dea_next",
-			LtsPath: "src/dea_next",
+			Name: "dea_next",
+			Path: "src/dea_next",
 		},
 		{
-			Name:    "smoke-tests",
-			Path:    "src/smoke-tests",
-			LtsPath: "src/smoke-tests",
+			Name: "smoke-tests",
+			Path: "src/smoke-tests",
 		},
 		{
-			Name:    "cf-acceptance-tests",
-			Path:    "src/github.com/cloudfoundry/cf-acceptance-tests/",
-			LtsPath: "src/github.com/cloudfoundry/cf-acceptance-tests/",
+			Name: "cf-acceptance-tests",
+			Path: "src/github.com/cloudfoundry/cf-acceptance-tests/",
 		},
 		{
-			Name:    "statsd-injector",
-			Path:    "src/statsd-injector",
-			LtsPath: "src/statsd-injector",
+			Name: "statsd-injector",
+			Path: "src/statsd-injector",
 		},
 		{
-			Name:    "hm9000",
-			Path:    "src/hm9000",
-			LtsPath: "src/hm9000",
+			Name: "hm9000",
+			Path: "src/hm9000",
 		},
 		{
-			Name:    "cf-routing-release",
-			Path:    "src/cf-routing-release",
-			LtsPath: "src/github.com/cloudfoundry-incubator/routing-api",
+			Name: "nats",
+			Path: "src/nats",
 		},
 		{
-			Name:    "nats",
-			Path:    "src/nats",
-			LtsPath: "src/nats",
-		},
-		{
-			Name:    "route-registrar",
-			Path:    "src/github.com/cloudfoundry-incubator/route-registrar",
-			LtsPath: "src/route_registrar",
+			Name: "route-registrar",
+			Path: "src/github.com/cloudfoundry-incubator/route-registrar",
 		},
 		{
 			Name: "consul-release",
 			Path: "src/consul-release",
 		},
 		{
-			Name:    "etcd",
-			Path:    "src/github.com/coreos/etcd",
-			LtsPath: "src/github.com/coreos/etcd",
+			Name: "etcd",
+			Path: "src/github.com/coreos/etcd",
 		},
 		{
-			Name:    "etcd-metrics-server",
-			Path:    "src/etcd-metrics-server",
-			LtsPath: "src/etcd-metrics-server",
+			Name: "etcd-metrics-server",
+			Path: "src/etcd-metrics-server",
 		},
 		{
-			Name:    "warden",
-			Path:    "src/warden/warden",
-			LtsPath: "src/warden/warden",
+			Name: "warden",
+			Path: "src/warden/warden",
 		},
 		{
-			Name:    "warden-client",
-			Path:    "src/warden/warden-client",
-			LtsPath: "src/warden/warden-client",
+			Name: "warden-client",
+			Path: "src/warden/warden-client",
 		},
 		{
-			Name:    "warden-protocol",
-			Path:    "src/warden/warden-protocol",
-			LtsPath: "src/warden/warden-protocol",
+			Name: "warden-protocol",
+			Path: "src/warden/warden-protocol",
 		},
 		{
 			Name: "em-posix-spawn",
