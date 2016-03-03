fd9dd8d5c9bfeab37eca7d6435eb5dc4b5f24780
Amin Jamali <ajamali@pivotal.io>
2016-03-03 16:54:45 -0500

    Adding cf-routing-release
    [Delivers #114701007]
    
    Signed-off-by: Micah Young <myoung@pivotal.io>

---
+++ cf-routing-release-submodules.go
@@ -0,0 +1,31 @@
+package main
+
+type CfRoutingReleaseSubmodules []struct {
+	Name string // the name of the project
+	Path string // the path of the project in the latest release
+}
+
+func (c CfRoutingReleaseSubmodules) Paths() []string {
+	p := make([]string, len(c))
+	for k, v := range c {
+		p[k] = v.Name + ":" + v.Path
+	}
+	return p
+}
+
+func NewCfRoutingReleaseSubmodules() CfRoutingReleaseSubmodules {
+	return CfRoutingReleaseSubmodules{
+		{
+			Name: "cf-tcp-router",
+			Path: "src/github.com/cloudfoundry-incubator/cf-tcp-router",
+		},
+		{
+			Name: "routing-api",
+			Path: "src/github.com/cloudfoundry-incubator/routing-api",
+		},
+		{
+			Name: "tcp-emitter",
+			Path: "src/github.com/cloudfoundry-incubator/tcp-emitter",
+		},
+	}
+}
